8309045eeb6f34dd5440cc0c3977a3c0d30107de
Anton Gorbunov <Anton_Gorbunov@rcomext.com>
2017-11-01 14:48:03 +0200

    webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
         rules: [
             {test: /\.js$/, use: ['ng-annotate-loader', 'babel-loader'], exclude: /node_modules/},
             {test: /\.html$/, use: 'html-loader'},
+            {test: /\.less$/, use: 'less-loader'},
             {test: /\.css$/, use: ['style-loader', 'css-loader']},
             {test: /\.(ttf|eot|svg|woff|woff(2)?)$/, use: 'file-loader?name=to-do/dist/fonts/[name].[ext]'},
             {test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader'}
