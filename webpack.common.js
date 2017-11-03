5966006235ed57ee20b70539e9628c24cbdfc4e4
Anton Gorbunov <Anton_Gorbunov@rcomext.com>
2017-11-03 15:05:27 +0200

    prod/dev configurations

---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {test: /\.js$/, use: ['ng-annotate-loader', 'babel-loader'], exclude: /node_modules/},
+            {test: /\.html$/, use: 'html-loader'},
+            {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']},
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {test: /\.(ttf|eot|svg|woff|woff(2)?)$/, use: 'file-loader?name=fonts/[name].[ext]'},
+            {test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader'}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            title: 'Production'
+        }),
+    ]
+};
