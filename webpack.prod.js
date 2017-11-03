0426271d2f4d14ceae62ef216a8d2e2564aea08e
Anton Gorbunov <Anton_Gorbunov@rcomext.com>
2017-11-03 15:55:56 +0200

    prod/dev configurations(plugin =( )

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,15 @@ const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = merge(common, {
     devtool: 'source-map',
     plugins: [
+        new ExtractTextPlugin('[name].css', {
+            allChunks: true,
+            publicPath: '/dist'
+        }),
         new UglifyJSPlugin({
             sourceMap: true
         }),
@@ -14,5 +19,19 @@ module.exports = merge(common, {
                 'NODE_ENV': JSON.stringify('production')
             }
         })
-    ]
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            }
+
+        ]
+    },
 });
