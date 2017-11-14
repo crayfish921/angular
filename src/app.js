89f9de892eb50a607d52c75025df9219474a8540
Anton Gorbunov <Anton_Gorbunov@rcomext.com>
2017-11-14 13:32:44 +0200

    remove clutter

--- src/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var angular = require("angular");
-var appModule = angular.module('todoApp', []);
-exports.default = appModule;
